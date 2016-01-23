cdeeccebd16cb52be6cca97d8de43edfe2aa5e98
piyushrungta25 <piyushrungta25@gmail.com>
2016-01-23 15:30:12 +0530

    minor fixes

--- post_management.py
+++ post_management.py
@@ -4,12 +4,19 @@ post_management = Blueprint('post_management', __name__)
 
 @post_management.route('/home', methods=['GET', 'POST'])
 def home():
-	if 'logged_in' not in session or 'username' not in session:
+	if 'logged_in' not in session or 'username' not in session or 'session_id' not in session:
 		session['messages'] =  "No active session"
 		return redirect(url_for('user_management.login'))
 		
-		# template for particular club
-	return render_template('home.html')
+	g.cur.execute("select session_id from login_data where email='%s'"%(session['email']))
+	result=g.cur.fetchall()
+	result=result[0][0]
+	if session['session_id']==result:
+		return render_template('home.html')
+	
+	else:
+		#pop all session entries
+		return redirect(url_for('login'))
 	
 @post_management.route('/temp', methods=['GET', 'POST'])
 def new_post():
